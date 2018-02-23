989d9df959fc229a9eb3758e7db390aa22a67922
Michael Campbell <michael.campbell@dvsa.gov.uk>
2018-02-23 15:54:33 +0000

    Add start page link

--- app/routes.js
+++ app/routes.js
@@ -24,25 +24,5 @@ router.post('/juggling-trick', function (req, res) {
 
 })
 
-//router.post('/check-your-answers', function (req, res) {
-
-  // Make a variable and give it the value from 'juggling-balls'
-  //var jugglinglocation = req.session.data['juggling-location']
-
-  // Check whether the variable matches a condition
-  //if (jugglinglocation == "England"){
-    // Send user to next page
-    //res.redirect('/Select-city-eng')
-  //} else if (jugglinglocation == "Scotland"){
-    // Send user to next page
-    //res.redirect('/Select-city-scot')
-  //} else if (jugglinglocation == "Wales"){
-    // Send user to next page
-    //res.redirect('/Select-city-wales')
-  //} else {
-    // Send user to ineligible page
-  //  res.redirect('Select-city-NI')
- // }
-//})
 
 module.exports = router
