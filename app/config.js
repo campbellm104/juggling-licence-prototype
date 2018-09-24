970b3a90dba2eb15517f4cfe142373feb4512557
Michael Campbell <campbellmichael104@gmail.com>
2018-09-24 10:45:45 +0100

    First commit

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for a Juggling licence',
+  serviceName: 'Find MOT location',
 
   // Default port that prototype runs on
   port: '3000',
